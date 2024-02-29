d4e8681706fa7b00e1dc8c6dec8fd5558d785276
cmenegoli <145634355+cmenegoli@users.noreply.github.com>
2024-02-28 21:13:07 -0600

    charts

--- piechart.js
+++ piechart.js
@@ -1,14 +1,17 @@
-
 let pie_url = 'http://127.0.0.1:3009/api/v1.0/pieChartDiv'
 
-// Data for the pie chart for Texas waste management
+// Data for the pie chart for Texas waste management with updated colors to match the bar chart
 var data = [{
-  values: [158974400, 16309950, 113585100, 22162810], // Corresponding values for Energy_Recovery, Recycling, Treatment, Total_Releases
+  values: [158974400, 16309950, 113585100, 22162810], // Corresponding values
   labels: ['Energy_Recovery', 'Recycling', 'Treatment', 'Total_Releases'], // Categories
   type: 'pie',
   hole: .4, // For a donut chart
   hoverinfo: 'label+percent',
-  textinfo: 'label+value'
+  textinfo: 'label+value',
+  marker: {
+    // Approximate colors based on the provided bar chart image
+    colors: ['#FFC0CB', '#DDA0DD', '#87CEFA', '#FFDAB9'] // pink, plum, lightskyblue, peachpuff
+  }
 }];
 
 var layout = {
